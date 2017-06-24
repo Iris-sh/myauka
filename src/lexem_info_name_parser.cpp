89911ef357df57fd4387a564cdcc570aa1b41543
gvs1977 <vladimir.s.gavrilov@gmail.com>
2017-06-24 17:06:06 +0300

    File with an implementation of the lexem info name parser was created.

---
+++ src/lexem_info_name_parser.cpp
@@ -0,0 +1,10 @@
+/*
+    File:    lexem_info_name_parser.cpp
+    Created: 24 June 2017 at 14:06 Moscow time
+    Author:  Гаврилов Владимир Сергеевич
+    E-mails: vladimir.s.gavrilov@gmail.com
+             gavrilov.vladimir.s@mail.ru
+             gavvs1977@yandex.ru
+*/
+
+#include "../include/lexem_info_name_parser.h"

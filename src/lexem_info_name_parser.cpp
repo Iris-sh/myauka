d8164eaa86d9c08f54d3962800f8150e0159a04e
gvs1977 <vladimir.s.gavrilov@gmail.com>
2017-06-25 11:45:11 +0300

    I began to write the function size_t Lexem_info_name_parser::compile().

--- src/lexem_info_name_parser.cpp
+++ src/lexem_info_name_parser.cpp
@@ -8,3 +8,35 @@
 */
 
 #include "../include/lexem_info_name_parser.h"
+
+enum class State{
+    Keyword, Name
+};
+
+size_t Lexem_info_name_parser::compile(){
+    size_t          ret_val = 0;
+    State           state   = State::Keyword;
+    while((lc = (li = msc_->current_lexem()).code)){
+        switch(state){
+//             case State::Addition:
+//                 if(lc == m){
+//                     state = State::Addition_string;
+//                 }else{
+//                     msc_->back();
+//                     return ret_val;
+//                 }
+//                 break;
+//             case State::Addition_string:
+//                 if(String == lc){
+//                     return li.string_index;
+//                 }else{
+//                     printf("Error at line %zu: the string literal was expected.\n",
+//                            msc_->lexem_begin_line_number() );
+//                     msc_->back();
+//                     return ret_val;
+//                 }
+//                 break;
+        }
+    }
+    return ret_val;
+}
